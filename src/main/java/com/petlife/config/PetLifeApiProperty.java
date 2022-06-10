d9a004d728902cd8ef41288656744f7aaf64e402
Pablo <pablodiegodemuner@gmail.com>
2022-06-09 20:58:17 -0400

    Implementado Cors]

---
+++ src/main/java/com/petlife/config/PetLifeApiProperty.java
@@ -0,0 +1,33 @@
+package com.petlife.config;
+
+public class PetLifeApiProperty {
+
+private final Seguranca seguranca = new Seguranca();
+	
+	private String origemPermitida = "http://localhost:4200";
+
+	public String getOrigemPermitida() {
+		return origemPermitida;
+	}
+
+	public void setOrigemPermitida(String origemPermitida) {
+		this.origemPermitida = origemPermitida;
+	}
+
+	public Seguranca getSeguranca() {
+		return seguranca;
+	}
+
+	public static class Seguranca {
+
+		private boolean enableHttps;
+
+		public boolean isEnableHttps() {
+			return enableHttps;
+		}
+
+		public void setEnableHttps(boolean enableHttps) {
+			this.enableHttps = enableHttps;
+		}
+	}
+}
